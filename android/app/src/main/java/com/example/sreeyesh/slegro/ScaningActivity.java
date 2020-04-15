e9e8de818c50fe2379afdedb8db4ae96997403c9
Sreeyesh Sreedharan <sreeyesh.sreedharan@harman.com>
2020-04-15 18:34:38 +0530

    Moved android app source code from master -> android_app_dev_br

---
+++ android/app/src/main/java/com/example/sreeyesh/slegro/ScaningActivity.java
@@ -0,0 +1,13 @@
+package com.example.sreeyesh.slegro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScaningActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scaning);
+    }
+}
